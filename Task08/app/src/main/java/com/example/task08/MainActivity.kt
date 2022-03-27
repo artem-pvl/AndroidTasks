d332a6a68f6ae7c2fbf6b14ff2f61b4aefb83b0c
Artem Pavlichenko <artem.pvl@gmail.com>
2022-03-27 14:42:11 +0300

    Task 08 done

--- Task08/app/src/main/java/com/example/task08/MainActivity.kt
+++ Task08/app/src/main/java/com/example/task08/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.task08
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtView = findViewById<TextView>(R.id.txtView)
+        val btnOk = findViewById<Button>(R.id.btnOk)
+
+        btnOk.setOnClickListener{
+            txtView.text = "Нажата кнопка"
+        }
     }
 }
