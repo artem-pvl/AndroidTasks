894555bdbdbe272bb4f7599f0d919af95d4393ea
Artem Pavlichenko <artem.pvl@gmail.com>
2022-04-12 16:09:58 +0500

    Task 14 done

--- Task14/app/src/main/java/com/example/task14/MainActivity.kt
+++ Task14/app/src/main/java/com/example/task14/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.task14
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task14.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onBtnAddClick()
+    }
+
+    private fun onBtnAddClick() {
+        binding.btnAdd.setOnClickListener {
+            binding.txtView.text = getString(R.string.text_on_btn_add_click)
+        }
     }
 }
