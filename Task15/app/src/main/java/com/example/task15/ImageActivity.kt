551f7e794de2a19612087fe7b119c5533a2087ca
Artem Pavlichenko <artem.pvl@gmail.com>
2022-04-15 10:30:21 +0500

    Task 15 binding moved into class, URL moved to const

--- Task15/app/src/main/java/com/example/task15/ImageActivity.kt
+++ Task15/app/src/main/java/com/example/task15/ImageActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.example.task15.databinding.ActivityImageBinding
 
-private lateinit var binding: ActivityImageBinding
+const val IMG_URL = "https://bigpicture.ru/wp-content/uploads/2012/04/2184.jpg"
 
 class ImageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityImageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityImageBinding.inflate(layoutInflater)
@@ -18,7 +20,7 @@ class ImageActivity : AppCompatActivity() {
 
     private fun showImage() {
         Glide.with(binding.root)
-            .load(getString(R.string.image_url))
+            .load(IMG_URL)
             .into(binding.imgView)
     }
 }
