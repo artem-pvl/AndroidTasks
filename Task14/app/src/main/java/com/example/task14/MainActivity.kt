84301557f9773f34abf89860cb1b80f95da826bb
Artem Pavlichenko <artem.pvl@gmail.com>
2022-04-15 10:16:07 +0500

    Task 14 var moved to class

--- Task14/app/src/main/java/com/example/task14/MainActivity.kt
+++ Task14/app/src/main/java/com/example/task14/MainActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.task14.databinding.ActivityMainBinding
 
-private lateinit var binding: ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
